088504353d2813c4de39e310a70afb20329a9ee0
Kiim94 <nemomoa@hotmail.com>
2026-06-01 16:30:12 +0200

    Ändrat vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,12 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
-        admin: resolve(__dirname, 'admin.html'),
+        adminMeny: resolve(__dirname, 'adminMeny.html'),
         loginAdmin: resolve(__dirname, 'loginAdmin.html'),
         meny: resolve(__dirname, 'meny.html'),
-        reviews: resolve(__dirname, 'reviews.html')
+        reviews: resolve(__dirname, 'reviews.html'),
+        delete: resolve(__dirname, 'delete.html'),
+        register: resolve(__dirname, 'register.html')
       }
     }
   }
